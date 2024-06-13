1b51cc1bdd85f6e4aaf987d44e6179d9290af64d
EmanRadwan114 <emanalaa11499@gmail.com>
2024-06-14 00:39:26 +0300

    some import paths were corrected

--- modules/user/user.routes.js
+++ modules/user/user.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { register, login, logout } from "./user.controllers.js";
+import { checkEmailExists } from "user.middleware..js";
 
 const userRouter = express.Router();
 
-userRouter.post("/register", register);
+userRouter.post("/register", checkEmailExists, register);
 userRouter.post("/login", login);
 userRouter.post("/logout", logout);
 
